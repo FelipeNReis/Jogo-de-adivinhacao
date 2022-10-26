95e3a9803c137d345e417bc9b6f4ecef6f062209
Felipe Neves dos Reis <103513622+FelipeNReis@users.noreply.github.com>
2022-10-26 13:34:17 -0300

    Arquivos do jogo

---
+++ forca.py
@@ -0,0 +1,11 @@
+def jogar():
+
+# Nome do jogo
+    print("******************************")
+    print("* Bem vindo ao jogo de Forca *")
+    print("******************************")
+
+    print("Fim do jogo!")
+
+if(__name__== "__main__"):
+    jogar()
